f852e5c64bc7055036396ceef47655257a01d4c3
Parth Panchal <parth.sdis@gmail.com>
2023-11-19 05:27:45 -0500

    final changes

--- test.py
+++ test.py
@@ -3,18 +3,18 @@ import os
 import sys
 
 
-def run_test(prog, name):
+def run_test(prog, name, args):
     input_file = f"test/{prog}.{name}.in"
     arg_expected_output_file = f"test/{prog}.{name}.arg.out"
-
     # Run the program with input as a command-line argument
+    cmd = ["python3", f"prog/{prog}.py", input_file]
+    if args:
+        cmd.append(args)
     result = subprocess.run(
-        ["python3", f"prog/{prog}.py", input_file],
+        cmd,
         text=True,
         capture_output=True,
     )
-
-    # Check for expected output from command-line argument
     if os.path.exists(arg_expected_output_file):
         with open(arg_expected_output_file, "r") as expected_out:
             expected_output = expected_out.read()
@@ -28,28 +28,60 @@ def run_test(prog, name):
                 print("\tgot:\n")
                 print(result.stdout)
                 return False
-
     print(f"OK: {prog} {name}")
     return True
 
 
 def main():
     failed_tests = 0
-    output_mismatch_count = 0
-
-    # Iterate over all files in the test directory
     for filename in os.listdir("test"):
         if filename.endswith(".in"):
-            prog, name = filename.split(".", 1)
-            success = run_test(prog, name.rstrip(".in"))
-            if not success:
-                failed_tests += 1
-                if "OutputMismatch" in str(sys.exc_info()[1]):
-                    output_mismatch_count += 1
-
-    print(f"\noutput mismatch: {output_mismatch_count}")
-    print(f"total: {failed_tests}")
-
+            if filename.startswith("wc"):
+                prog, name = filename.split(".", 1)
+                if name == "testcase1.in":
+                    success = run_test(prog, name.rstrip(".in"), "")
+                    if not success:
+                        failed_tests += 1
+                if name == "testcase2.in":
+                    success = run_test(prog, name.rstrip(".in"), "-l")
+                    if not success:
+                        failed_tests += 1
+                if name == "testcase3.in":
+                    success = run_test(prog, name.rstrip(".in"), "-wc")
+                    if not success:
+                        failed_tests += 1
+            if filename.startswith("gron"):
+                prog, name = filename.split(".", 1)
+                if name == "testcase1.in":
+                    success = run_test(prog, name.rstrip(".in"), "")
+                    if not success:
+                        failed_tests += 1
+                if name == "testcase2.in":
+                    success = run_test(prog, name.rstrip(".in"), "")
+                    if not success:
+                        failed_tests += 1
+                if name == "testcase3.in":
+                    success = run_test(prog, name.rstrip(".in"), "--obj o")
+                    if not success:
+                        failed_tests += 1
+            if filename.startswith("csv_sum"):
+                if name == "testcase1.in":
+                    success = run_test(prog, name.rstrip(".in"), "Age Salary")
+                    if not success:
+                        failed_tests += 1
+                if name == "testcase2.in":
+                    success = run_test(prog, name.rstrip(".in"), "Age")
+                    if not success:
+                        failed_tests += 1
+                if name == "testcase3.in":
+                    success = run_test(prog, name.rstrip(".in"), "Salary")
+                    if not success:
+                        failed_tests += 1
+                prog, name = filename.split(".", 1)
+                success = run_test(prog, name.rstrip(".in"), "")
+                if not success:
+                    failed_tests += 1
+    print(f"\nfailed tests: {failed_tests}")
     if failed_tests > 0:
         sys.exit(1)
     else:
